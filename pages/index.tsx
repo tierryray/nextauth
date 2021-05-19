2b97052b407956d8dd9ceebddfdcd5f933820579
Tierry Ray <tierry.ray@ifood.com.br>
2021-05-19 09:58:41 -0300

    feat(index): access cookie from server side and redirect if user has token

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import { FormEvent, useContext, useState } from 'react'
 
 import { AuthContext } from '../contexts/AuthContext';
+import { GetServerSideProps } from 'next';
+import { parseCookies } from 'nookies';
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -28,4 +30,21 @@ export default function Home() {
       <button type="submit">Entrar</button>
     </form>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const cookies = parseCookies(ctx);
+
+  if (cookies['nextauth.token']) {
+    return {
+      redirect: {
+        destination: '/dashboard',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {},
+  }
 }
