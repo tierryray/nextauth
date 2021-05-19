e7fe9a52a6f4aea46517d5a86303252337c8b20d
Tierry Ray <tierry.ray@ifood.com.br>
2021-05-19 11:21:47 -0300

    feat(utils): validateUserPermissions

--- hooks/useCan.ts
+++ hooks/useCan.ts
@@ -1,5 +1,6 @@
 import { AuthContext } from "../contexts/AuthContext";
 import { useContext } from "react";
+import { validateUserPermissions } from "../utils/validateUserPermissions";
 
 type UserCanParams = {
   permissions?: string[];
@@ -11,17 +12,11 @@ export function useCan({ permissions, roles } : UserCanParams) {
 
   if (!isAuthenticated) return false;
 
-  if (permissions?.length > 0) {
-    const hasAllPermissions = permissions.every(permission =>  user.permissions.includes(permission));
+  const userHasValidPermissions = validateUserPermissions({
+    user,
+    permissions,
+    roles,
+  });
 
-    if (!hasAllPermissions) return false;
-  }
-
-  if (roles?.length > 0) {
-    const hasAllRoles = roles.some(permission =>  user.roles.includes(permission));
-
-    if (!hasAllRoles) return false;
-  }
-
-  return true;
+  return userHasValidPermissions;
 }
