c6ff00e347987948ee7d26af68c88462166683c4
Tierry Ray <tierry.ray@ifood.com.br>
2021-05-19 11:07:43 -0300

    feat(components): create boolean component to show or not children

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,17 +1,13 @@
 import { useContext, useEffect } from "react"
 
 import { AuthContext } from "../contexts/AuthContext"
+import { Can } from "../components/Can";
 import { api } from '../services/apiClient';
 import { setupAPIClient } from "../services/api";
-import { useCan } from "../hooks/useCan";
 import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
-
-  const userCanSeeMetrics = useCan({
-    permissions: ['metrics.list'],
-  });
 
   useEffect(() => {
     api.get('/me').then(response => console.log(response))
@@ -22,7 +18,9 @@ export default function Dashboard() {
     <>
       <h1>Dashboard: {user?.email}</h1>
 
-      { userCanSeeMetrics && <div>Métricas</div> }
+      <Can permissions={['metrics.list']}>
+        <div>Métricas</div>
+      </Can>
     </>
   )
 };
